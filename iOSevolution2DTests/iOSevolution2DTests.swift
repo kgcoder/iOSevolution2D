ed4a4aebdcdc1b3fd3fd2f041a2b63d7c16d9e6f
Karen Grigoryan <karengrig@mail.ru>
2015-03-21 18:23:44 +0300

    Initial Commit

---
+++ iOSevolution2DTests/iOSevolution2DTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOSevolution2DTests.swift
+//  iOSevolution2DTests
+//
+//  Created by Карен Григорян on 21/03/15.
+//  Copyright (c) 2015 ___KAREN___. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class iOSevolution2DTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
